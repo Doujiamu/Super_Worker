3a7ee4379a9f119bd9fd86069058fc5477b66ffe
Li Junhui <104183798+Doujiamu@users.noreply.github.com>
2023-03-07 12:45:27 +0800

    Create setMap.c

--- modTool/setMap.c
+++ modTool/setMap.c
@@ -1,8 +1,22 @@
+//This programme is used for settingmaps automatically
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void){
   FILE *fp = NULL;
+  int block,lenX,LenY;
+  int buf;
   
+  fp ＝ fopen("settingmap.txt","w+");
+
+  print("please input: blocks-x-y:")
+  scanf("%d-%d-%d",&block,&lenX,&LenY);
+
+  do{
+
+  }while(buf==10086);
+
+  fclose(fp); 
   return 0;
 }
